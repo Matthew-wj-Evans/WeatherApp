47dfce8c2d215292282b2c80a1015ad2022b1844
Matthew Evans <54965717+Matthew-wj-Evans@users.noreply.github.com>
2022-02-07 12:46:52 -0700

    no longer pushes API key to public repo

--- NetworkAPI.cpp
+++ NetworkAPI.cpp
@@ -1,13 +1,19 @@
 #ifndef NETWORKAPI_CPP
 #define NETWORKAPI_CPP
+#include <bits/stdc++.h>
+
 /*
     This class is responsible for getting and returning HTTP requests
     all data returned will be 
 */
 class NetworkAPI {
-    // Private members
+        wchar_t _base_path[128];
+        wchar_t _lat_arg[128];
+        wchar_t _prefix_lon_arg[128];
+        wchar_t _app_id_arg[128];
     public:
-
+        wchar_t* GetURL();
+        bool SetURL(wchar_t[], wchar_t[], wchar_t[], wchar_t[]);
 } network;
 
 #endif
